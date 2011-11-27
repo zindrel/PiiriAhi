232227829ef990e1539b42741653bb5d8f329cf3
Robin Soon <robin.soon@gmail.com>
2011-11-27 16:01:50 +0200

    removed unused imports

--- PiiriAhi/src/main/java/ee/itcollege/team24/entities/Piiriloik.java
+++ PiiriAhi/src/main/java/ee/itcollege/team24/entities/Piiriloik.java
@@ -4,10 +4,6 @@ import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.tostring.RooToString;
 import javax.validation.constraints.NotNull;
-import java.util.Calendar;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @RooJavaBean
 @RooToString
