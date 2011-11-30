28cc0660e8b4fabcf101d23bc74acb2d7aa36cf5
Robin Soon <robin.soon@gmail.com>
2011-12-01 00:55:49 +0200

    Going back to index for register incident

--- PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
+++ PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
@@ -3,17 +3,13 @@ package ee.itcollege.team24.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import ee.itcollege.team24.entities.Intsident;
-
-@RooWebScaffold(path = "registerincident", formBackingObject = Intsident.class)
-@RequestMapping("/registerincident")
+@RequestMapping("/registerincident/**")
 @Controller
 public class RegisterIncidentController {
 
