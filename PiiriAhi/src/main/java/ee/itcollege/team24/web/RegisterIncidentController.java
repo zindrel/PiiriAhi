bacea28c0ed4aeb9fdda6279e0e8c7d15ed1f0e8
Taavi Jakoobi <taavi.jakoobi@gmail.com>
2011-12-01 03:02:55 +0200

    Moving back to Roo generated form. Cancel button implementation!

--- PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
+++ PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
@@ -3,13 +3,18 @@ package ee.itcollege.team24.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/registerincident/**")
+import ee.itcollege.team24.entities.Intsident;
+import ee.itcollege.team24.entities.Vaeosa;
+
+@RooWebScaffold(path = "registerincident", formBackingObject = Intsident.class)
+@RequestMapping("/registerincident")
 @Controller
 public class RegisterIncidentController {
 
