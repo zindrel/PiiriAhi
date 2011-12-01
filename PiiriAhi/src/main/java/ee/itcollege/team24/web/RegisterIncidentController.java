7550d86eb525bd6123d689bc33823b42fb67e45c
Robin Soon <robin.soon@gmail.com>
2011-12-01 03:40:05 +0200

    Removed unused index.jspx

--- PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
+++ PiiriAhi/src/main/java/ee/itcollege/team24/web/RegisterIncidentController.java
@@ -1,14 +1,8 @@
 package ee.itcollege.team24.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import ee.itcollege.team24.entities.Intsident;
 
@@ -17,16 +11,4 @@ import ee.itcollege.team24.entities.Intsident;
 @Controller
 public class RegisterIncidentController {
 
-    @RequestMapping
-    public void get(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "{id}")
-    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    @RequestMapping
-    public String index() {
-        return "registerincident/index";
-    }
 }
